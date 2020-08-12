c7657e5f703a4c55b687cf42747779b1af152bee
Juan Morena <juanmmorena@gmail.com>
2020-08-12 11:05:38 -0300

    a ver si ahora anda esta poronga

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,87 @@
 import { AssistantPackage, RuleDefinition } from '@sketch-hq/sketch-assistant-types'
 
-const helloWorld: RuleDefinition = {
-  rule: async (context) => {
-    context.utils.report('Hello world')
-  },
-  name: 'sketch-assistant-template/hello-world',
-  title: 'Hello World',
-  description: 'Reports a hello world message',
+const INCREMENTS: { [key: string]: string[] } = {
+  '@1x': ['4.00'],
 }
+
+// const isRotated = (object: unknown) => isSketchFileObject(object) && 'rotation' in object && object.rotation !== 0
+
+const parseIncrements = (scaleFactors: unknown): string[] => {
+  if (!scaleFactors || !Array.isArray(scaleFactors)) return []
+  let validIncrements: string[] = []
+  for (let i = 0; i < scaleFactors.length; i++) {
+    const scaleFactor = scaleFactors[i]
+    if (typeof scaleFactor === 'string') {
+      const increments = INCREMENTS[scaleFactor]
+      if (Array.isArray(increments)) {
+        validIncrements = [...validIncrements, ...increments]
+      }
+    }
+  }
+  return [...new Set(validIncrements)]
+}
+
+export const createRule: CreateRuleFunction = (i18n) => {
+  const rule: RuleFunction = async (context: RuleContext): Promise<void> => {
+    const { utils } = context
+    const increments = parseIncrements(utils.getOption('scaleFactors'))
+    for (const layer of utils.objects.anyLayer) {
+      if (!('frame' in layer)) continue // Narrow type to layers with a `frame` prop
+
+      // If the current layer or any of its parents have rotation skip this loop
+      const rotatedLayers = [...utils.getObjectParents(layer), layer]
+        .filter(isSketchFileObject)
+        .filter((object) => 'rotation' in object && object.rotation !== 0)
+      if (rotatedLayers.length > 0) continue
+
+      let { x, y } = layer.frame
+      // Round x,y values to two decimal places to mimick how Sketch normalises
+      // values too. This avoids reporting subpixel violations that don't match
+      // with what Sketch displays in the inspector
+      x = Math.round(x * 100) / 100
+      y = Math.round(y * 100) / 100
+      // Convert x,y values to increment values (e.g `12.5` to `0.50`) and check
+      // to see if they're valid
+      const xValid = increments.includes(Math.abs(x % 1).toFixed(2))
+      const yValid = increments.includes(Math.abs(y % 1).toFixed(2))
+      if (!xValid || !yValid) {
+        utils.report(i18n._(t`Unexpected sub-pixel positioning (${x}, ${y})`), layer)
+      }
+    }
+  }
+
+  return {
+    rule,
+    name: 'Ava16',
+    title: (ruleConfig) => {
+      const increments = parseIncrements(ruleConfig.scaleFactors as string[]).join(', ')
+      return i18n._(t`Layers must be placed on ${increments} sub-pixel increments`)
+    },
+    description: i18n._(
+      t`Some teams may consider layers place on sub-pixel values a document organization issue. The exception is if you're designing for devices with @2x and @3x pixel density. In this case, you can use 0.5 and 0.33/0.67 increments.`,
+    ),
+    getOptions(helpers) {
+      return [
+        helpers.stringArrayOption({
+          name: 'scaleFactors',
+          title: i18n._(t`Scale Factors`),
+          description: i18n._(
+            t`Choose the scale factors that the document supports. Accepted values are @1x, @2x and @3x, which allow whole pixel positions, 0.5 increments and 0.33 increments respectively`,
+          ),
+          minLength: 1,
+          maxLength: 3,
+          pattern: '^@[1-3]x$',
+        }),
+      ]
+    },
 
 const assistant: AssistantPackage = async () => {
   return {
-    name: 'sketch-assistant-template',
-    rules: [helloWorld],
+    name: 'Ava 16px',
+    rules: [Ava16],
     config: {
       rules: {
-        'sketch-assistant-template/hello-world': { active: true },
+        'sketch-assistant-template/Ava16': { active: true },
       },
     },
   }
